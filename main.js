013efa96095e7151b8f6434c1bfe99890ebad19e
Marco <posta.email3001@gmail.com>
2022-11-11 17:11:54 +0100

    js_text

--- main.js
+++ main.js
@@ -1,6 +1,38 @@
 //dato un array contenente tot immagini creare uno slideshow
 
-const items = document.getElementsByClassName("item");
+//crea array immagini
+const items = ["img/01.webp","img/02.webp","img/03.webp","img/04.webp","img/05.webp"]
 console.log(items);
 
-let activeItem = 0;
+//inserisci array in html
+const container = document.getElementById("container");
+
+for (let i = 0; i < items.length; i++) {
+    
+    container.innerHTML = `<figure class="item active"><img src="${items[i]}"></figure>`;
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+//navigazione
+/*const next = document.querySelector(".next");
+let item = 0;
+
+next.addEventListener("click",
+
+    function(){
+        if (item < items.lenght -1) {
+            items[item].c
+        }
+    }
+
+);*/
